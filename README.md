5cc27e69b0b55b1a1d5959bad268b8f0ad36be14
Prayuj Tuli <82406637+prayujt@users.noreply.github.com>
2022-09-30 15:33:26 -0400

    Added README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-* Plaid Sync
+# Plaid Sync
+This repository will utilize the Plaid API to generate [beancount](https://github.com/beancount/beancount) files to automate the process of tracking personal expenses from both bank accounts and credit cards. Using the API, we can easily get transaction information, categorize transactions by their type (eg. restaurants, groceries, etc..), and synchronize this information across multiple types of accounts into one beancount file.
+
+## Setup
+Requires a .env file within the main directory to be set with the following environment variables:
+* `CLIENT_ID` -> Plaid API application client ID
+* `SECRET` -> Plaid API application secret for development mode
+* `ACCESS_TOKENS` -> Access tokens for Plaid links (more detail below)
+* `START_DATE` -> Date to begin tracking transactions (%Y-%m-%d)
+
+To retrieve the access tokens for linked accounts, clone [Plaid's Quickstart](https://github.com/plaid/quickstart) repository, and follow the instructions in the README to link your accounts and get access tokens.
+
--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 from dotenv import dotenv_values
 import plaid
 from plaid.api import plaid_api
-from plaid.model.transactions_sync_request import TransactionsSyncRequest
 from datetime import datetime
 from plaid.model.transactions_get_request_options import TransactionsGetRequestOptions
 from plaid.model.transactions_get_request import TransactionsGetRequest
@@ -18,36 +17,20 @@ configuration = plaid.Configuration(
     }
 )
 
-access_token_discover = config["DISCOVER_ACCESS_TOKEN"]
-access_token_boa = config["BOA_ACCESS_TOKEN"]
+tokens = config["ACCESS_TOKENS"].split(' ')
+start_date = config["START_DATE"]
+
+access_token_discover = tokens[0]
+access_token_boa = tokens[1]
 
 access_token = access_token_discover
 
 api_client = plaid.ApiClient(configuration)
 client = plaid_api.PlaidApi(api_client)
 
-# request = TransactionsSyncRequest(
-#     access_token=access_token,
-# )
-# response = client.transactions_sync(request)
-# transactions = response['added']
-# print(response['next_cursor'])
-
-# while (response['has_more']):
-#     request = TransactionsSyncRequest(
-#         access_token=access_token,
-#         cursor=response['next_cursor']
-#     )
-#     response = client.transactions_sync(request)
-#     transactions += response['added']
-
-# for transaction in transactions:
-    # print(transaction)
-    # print(str(transaction['date']))
-
 request = TransactionsGetRequest(
     access_token=access_token,
-    start_date=datetime.strptime('2022-09-24', '%Y-%m-%d').date(),
+    start_date=datetime.strptime(start_date, '%Y-%m-%d').date(),
     end_date=datetime.strptime('2050-12-01', '%Y-%m-%d').date(),
 )
 response = client.transactions_get(request)
@@ -59,12 +42,12 @@ while len(transactions) < response['total_transactions']:
 
     request = TransactionsGetRequest(
         access_token=access_token,
-        start_date=datetime.strptime('2022-09-24', '%Y-%m-%d').date(),
+        start_date=datetime.strptime(start_date, '%Y-%m-%d').date(),
         end_date=datetime.strptime('2050-12-01', '%Y-%m-%d').date(),
         options=options
     )
     response = client.transactions_get(request)
     transactions += response['transactions']
 
-for transaction in transactions:
-    print(transaction['date'])
+# for transaction in transactions:
+    # print(transaction['date'])
