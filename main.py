5517f6d4f16452d58ed22bd64cafb115bf103f60
Prayuj Tuli <82406637+prayujt@users.noreply.github.com>
2022-09-30 16:21:58 -0400

    Working retrieval of institution name from access token

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 from dotenv import dotenv_values
+from datetime import datetime, timedelta
+
 import plaid
 from plaid.api import plaid_api
-from datetime import datetime
-from plaid.model.transactions_get_request_options import TransactionsGetRequestOptions
+
+from plaid.model.country_code import CountryCode
+
+from plaid.model.transactions_get_request_options \
+    import TransactionsGetRequestOptions
 from plaid.model.transactions_get_request import TransactionsGetRequest
 
+from plaid.model.accounts_get_request import AccountsGetRequest
+
+from plaid.model.institutions_get_by_id_request \
+    import InstitutionsGetByIdRequest
+
 config = dotenv_values()
+
+access_tokens = {}
 
 configuration = plaid.Configuration(
     host=plaid.Environment.Development,
@@ -17,37 +29,53 @@ configuration = plaid.Configuration(
     }
 )
 
-tokens = config["ACCESS_TOKENS"].split(' ')
 start_date = config["START_DATE"]
 
-access_token_discover = tokens[0]
-access_token_boa = tokens[1]
+# keep the end date always one ahead of the current date to retrieve
+# every transaction to date
+end_date = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
 
-access_token = access_token_discover
 
 api_client = plaid.ApiClient(configuration)
 client = plaid_api.PlaidApi(api_client)
 
-request = TransactionsGetRequest(
-    access_token=access_token,
-    start_date=datetime.strptime(start_date, '%Y-%m-%d').date(),
-    end_date=datetime.strptime('2050-12-01', '%Y-%m-%d').date(),
-)
-response = client.transactions_get(request)
-transactions = response['transactions']
+# for each access token supplied, map the institution name to that token
+for token in config["ACCESS_TOKENS"].split(' '):
+    request = AccountsGetRequest(access_token=token)
+    response = client.accounts_get(request)
+    institution_id = response['item']['institution_id']
+    request = InstitutionsGetByIdRequest(
+        institution_id=institution_id,
+        country_codes=[CountryCode("US")],
+    )
+    response = client.institutions_get_by_id(request)
+    access_tokens[response['institution']['name']] = token
 
-while len(transactions) < response['total_transactions']:
-    options = TransactionsGetRequestOptions()
-    options.offset = len(transactions)
-
+for (institution, access_token) in access_tokens.items():
+    print(institution + ":")
     request = TransactionsGetRequest(
         access_token=access_token,
         start_date=datetime.strptime(start_date, '%Y-%m-%d').date(),
-        end_date=datetime.strptime('2050-12-01', '%Y-%m-%d').date(),
-        options=options
+        end_date=datetime.strptime(end_date, '%Y-%m-%d').date(),
     )
     response = client.transactions_get(request)
-    transactions += response['transactions']
+    transactions = response['transactions']
 
-# for transaction in transactions:
-    # print(transaction['date'])
+    while len(transactions) < response['total_transactions']:
+        options = TransactionsGetRequestOptions()
+        options.offset = len(transactions)
+
+        request = TransactionsGetRequest(
+            access_token=access_token,
+            start_date=datetime.strptime(start_date, '%Y-%m-%d').date(),
+            end_date=datetime.strptime(end_date, '%Y-%m-%d').date(),
+            options=options
+        )
+        response = client.transactions_get(request)
+        transactions += response['transactions']
+
+    for transaction in transactions:
+        name = transaction['name'] if transaction['merchant_name'] is None \
+            else transaction['merchant_name']
+        print(transaction['date'], name, transaction['amount'])
+    print()
