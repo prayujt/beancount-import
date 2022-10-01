a09d53e7c5239be94f9d2c89c7cd9c51b1ee1029
Prayuj Tuli <82406637+prayujt@users.noreply.github.com>
2022-10-01 17:47:35 -0400

    Started basic conversion to .beancount format

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ connection = Connection(
     config["SECRET"]
 )
 
+
+beancount_file = open('transactions.beancount', 'w')
+
 for transaction in connection.get_transactions(start_date):
     name = transaction['name'] if transaction['merchant_name'] is None \
         else transaction['merchant_name']
@@ -23,3 +26,11 @@ for transaction in connection.get_transactions(start_date):
         name,
         transaction['amount']
     )
+    line1 = "{0} * \"{1}\"\n".format(start_date, name)
+    line2 = "\tAssets:BoA:Checking\t-{0} USD\n".format(transaction['amount'])
+    line3 = "\t Expenses:General\t{0} USD\n".format(transaction['amount'])
+    beancount_file.write(line1)
+    beancount_file.write(line2)
+    beancount_file.write(line3)
+
+beancount_file.close()
